2b03d2aa59b7287fb65ebbc1ce5d17a029af4aa7
GANGUMOLU LAKSHMI DAKSHINA SRI PRADHAN <2100040155ece@gmail.com>
2025-04-29 07:21:42 +0530

    Updated production build with new backend URL

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/predict", {
+      const response = await fetch("https://predictive-maintenance-backend.onrender.com/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
